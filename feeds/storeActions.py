ae04ee78f545812f1a36b927e10538872da04473
Grégory Paul <paulgreg@gmail.com>
2013-10-27 09:29:30 +0100

    Some views refactor

---
+++ feeds/storeActions.py
@@ -0,0 +1,48 @@
+from models import FeedEntry
+from converter import Converter
+from urlfetcher import UrlFetcher
+
+import logging
+
+class StoreActions():
+
+  def __init__(self):
+    self.converter = Converter()
+    self.urlFetcher = UrlFetcher()
+
+  def addUrl(self, url, name, title, description):
+    formatedUrl = url.replace('&', '&amp;')
+  
+    if name == 'somename' and not url.startswith('http://en.wikipedia.org/'):
+      return # Spam protection for default feed name
+  
+    lastFeedEntries = FeedEntry.objects.filter(name=name).order_by('-creation_date')
+  
+    if len(lastFeedEntries) == 0 or formatedUrl != lastFeedEntries[0].url: # Do not add same URL twice !
+  
+      feedentry = FeedEntry(url=formatedUrl, name=name)
+      feedentry.description = self.converter.convert(description)
+  
+      if not title:
+          try:
+            title = self.urlFetcher.fetch(url, '(?<=<(title|TITLE)>)[^<|^\r|^\n]*')
+          except Exception, err:
+            logging.exception('Error while fetching page title:')
+            feedentry.title = feedentry.url
+  
+      feedentry.title = self.converter.convert(title)
+  
+      if not feedentry.title:
+          feedentry.title = feedentry.url
+  
+      feedentry.save()
+  
+  
+  def deleteUrl(self, url, name):
+    formatedUrl = url.replace('&', '&amp;')
+  
+    feed = FeedEntry.objects.filter(name=name, url=url).order_by('-creation_date')
+  
+    if len(feed) >= 1:
+      feed[0].delete()
+  
--- feeds/views.py
+++ feeds/views.py
@@ -1,31 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import redirect
-from django.utils import timezone
 
 import logging
 
 from models import FeedEntry
-from converter import Converter
 from urlfetcher import UrlFetcher
 from feedNameCleaner import FeedNameCleaner
 from limitParser import LimitParser
 
+from storeActions import StoreActions
+
+
+feedNameCleaner = FeedNameCleaner()
+urlFetcher = UrlFetcher()
+limitParser = LimitParser()
+storeActions = StoreActions()
+
 
 def index(request):
-  feedNameCleaner = FeedNameCleaner()
   name = feedNameCleaner.clean(request.GET.get('name') or request.GET.get('n'))
   if not name:
     return renderHome(request)
   else:
-    limitParser = LimitParser()
     limit = limitParser.parse(request.GET.get('limit') or request.GET.get('l'))
     return renderRss(request, name, limit)
 
 
 def renderHome(request):
   try:
-    urlFetcher = UrlFetcher()
     random_url = urlFetcher.fetch('http://en.wikipedia.org/w/index.php?title=Special:RecentChanges&feed=rss', '(?<=<link>)http://en.wikipedia.org/w/index.php?[^&]*')
   except Exception, err:
     logging.exception('Error while fetching example URL:')
@@ -42,7 +45,6 @@ def renderRss(request, name, limit):
 
 
 def add(request): 
-  feedNameCleaner = FeedNameCleaner()
   name = feedNameCleaner.clean(request.GET.get('name') or request.GET.get('n'))
   url = request.GET.get('url') or request.GET.get('u')
   if not name or not url:
@@ -50,42 +52,12 @@ def add(request):
   else:
     title  = request.GET.get('title') or request.GET.get('t')
     description = request.GET.get('description') or request.GET.get('d')
-    addUrl(url, name, title, description)
+    storeActions.addUrl(url, name, title, description)
 
     return HttpResponseRedirect('/?name=' + name)
 
 
-def addUrl(url, name, title, description):
-  converter = Converter()
-  formatedUrl = url.replace('&', '&amp;')
-
-  if name == 'somename' and not url.startswith('http://en.wikipedia.org/'):
-    return # Spam protection for default feed name
-
-  lastFeedEntries = FeedEntry.objects.filter(name=name).order_by('-creation_date')
-
-  if len(lastFeedEntries) == 0 or formatedUrl != lastFeedEntries[0].url: # Do not add same URL twice !
-
-    feedentry = FeedEntry(url=formatedUrl, name=name, creation_date=timezone.now())
-    feedentry.description = converter.convert(description)
-
-    if not title:
-        try:
-          urlFetcher = UrlFetcher()
-          title = urlFetcher.fetch(url, '(?<=<(title|TITLE)>)[^<|^\r|^\n]*')
-        except Exception, err:
-          logging.exception('Error while fetching page title:')
-          feedentry.title = feedentry.url
-
-    feedentry.title = converter.convert(title)
-
-    if not feedentry.title:
-        feedentry.title = feedentry.url
-
-    feedentry.save()
-
 def delete(request): 
-  feedNameCleaner = FeedNameCleaner()
   name = feedNameCleaner.clean(request.GET.get('name') or request.GET.get('n'))
   if not name:
     return renderHome(request)
@@ -93,16 +65,8 @@ def delete(request):
     url = request.GET.get('url') or request.GET.get('u')
 
     if url:
-      deleteUrl(url, name)
+      storeActions.deleteUrl(url, name)
 
     return HttpResponseRedirect('/?name=' + name)
 
-
-def deleteUrl(url, name):
-  formatedUrl = url.replace('&', '&amp;')
-
-  feed = FeedEntry.objects.filter(name=name, url=url).order_by('-creation_date')
-
-  if len(feed) >= 1:
-    feed[0].delete()
 

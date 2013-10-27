f52af8f5dcea1900d94775c66e2ee1151b8902b0
Grégory Paul <paulgreg@gmail.com>
2013-10-27 09:51:08 +0100

    Update home page

--- rsstodolist/urls.py
+++ rsstodolist/urls.py
@@ -1,16 +1,8 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#from feeds.models import FeedEntry
-
-#admin.autodiscover()
-#admin.site.register(FeedEntry)
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rsstodolist.views.home', name='home'),
-
     url(r'', include('feeds.urls')),
-    #url(r'^admin/', include(admin.site.urls))
 )
+
+urlpatterns += staticfiles_urlpatterns()

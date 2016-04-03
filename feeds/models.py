96de894a17c0c445b7032cbeda3a4d91d7f49af1
Grégory Paul <paulgreg@gmail.com>
2016-04-03 10:55:45 +0200

    Change url length

--- feeds/models.py
+++ feeds/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class FeedEntry(models.Model):
   name = models.CharField(max_length=20)
-  url = models.CharField(max_length=255)
+  url = models.CharField(max_length=512)
   title = models.CharField(max_length=255,null=True)
   creation_date = models.DateTimeField(auto_now=True)
   description = models.TextField(null=True)
